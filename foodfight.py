2eda7a9e986ede1257144f0da4d26efcb5a24281
Daniel Bordak <dbordak@gmail.com>
2014-04-06 07:53:54 -0400

    Moved stuff around

--- foodfight.py
+++ foodfight.py
@@ -34,6 +34,18 @@ def getAccount(playerNo):
     print api.get_account_info(email,pw)
     return email,pw
 
+def makeOrder(menu,winner,loser,tray_string):
+    if winner == 1:
+        print "We're going to do order 1 using user 2"
+    else:
+        print "We're going to do order 2 using user 1"
+    order = list()
+    items = tray_string.split(",")
+    for item in items:
+        nums = item.split(":")
+        items.append(menu[nums[0]]['children'][nums[1]]['id'])
+
+
 def main():
     if len(sys.argv) < 2:
         print "Incorrect number of args."
@@ -41,64 +53,59 @@ def main():
     game = os.path.relpath(sys.argv[1])
     print "Note: If applicable, the resident of your current location"
     print "should be player 1."
-    #print "Does player 1 have an account? [Y/n]"
-    #ans = raw_input().lower()
-    #if ans == "n" or ans == "no":
-    #    player1 = createAccount("1")
-    #else:
-    #    player1 = getAccount("1")
-    #print "Does player 2 have an account? [Y/n]"
-    #ans = raw_input().lower()
-    #if ans == "n" or ans == "no":
-    #    player2 = createAccount("2")
-    #else:
-    #    player2 = getAccount("2")
-    #print "What address are you at?"
-    #street = raw_input()
-    #print "What city are you in?"
-    #city = raw_input()
-    #print "What is your Zip Code?"
-    #zip = raw_input()
-    #print "What is your quest?"
-    #raw_input()
-    #list = api.delivery_list("ASAP",street,city,zip)
-    #print "Restaurants Available:"
-    #num = 0
-    #for restaurant in list:
-    #    print str(num) + " " + restaurant['na']
-    #    num+=1
-    #print ""
-    #print "What is your selection? "
-    #choice = raw_input()
-    #print "You chose " + list[int(choice)]['na']
-    #menu = api.restaurant_details(str(list[int(choice)]['id']))['menu']
-    #print "Items on the menu:"
-    #num1 = 0
-    #for category in menu:
-    #    num2=0
-    #    print "Category "+str(num1)+": "+category['name']
-    #    for item in category['children']:
-    #        print str(num1)+":"+str(num2) + " " + item['name'] + ": $" + item['price']
-    #        num2+=1
-    #    num1+=1
-    #print ""
-    #print "To order multiple items, separate each with a comma (e.g. 0:2,1:3)"
-    #print ""
-    #print "Player 1, what is your choice?"
-    #tray_string1 = raw_input()
-    #print "Player 2, what is your choice?"
-    #tray_string2 = raw_input()
+    print "Does player 1 have an account? [Y/n]"
+    ans = raw_input().lower()
+    if ans == "n" or ans == "no":
+        player1 = createAccount("1")
+    else:
+        player1 = getAccount("1")
+    print "Does player 2 have an account? [Y/n]"
+    ans = raw_input().lower()
+    if ans == "n" or ans == "no":
+        player2 = createAccount("2")
+    else:
+        player2 = getAccount("2")
+    print "What address are you at?"
+    street = raw_input()
+    print "What city are you in?"
+    city = raw_input()
+    print "What is your Zip Code?"
+    zip = raw_input()
+    print "What is your quest?"
+    raw_input()
+    list = api.delivery_list("ASAP",street,city,zip)
+    print "Restaurants Available:"
+    num = 0
+    for restaurant in list:
+        print str(num) + " " + restaurant['na']
+        num+=1
+    print ""
+    print "What is your selection? "
+    choice = raw_input()
+    print "You chose " + list[int(choice)]['na']
+    menu = api.restaurant_details(str(list[int(choice)]['id']))['menu']
+    print "Items on the menu:"
+    num1 = 0
+    for category in menu:
+        num2=0
+        print "Category "+str(num1)+": "+category['name']
+        for item in category['children']:
+            print str(num1)+":"+str(num2) + " " + item['name'] + ": $" + item['price']
+            num2+=1
+        num1+=1
+    print ""
+    print "To order multiple items, separate each with a comma (e.g. 0:2,1:3)"
+    print ""
+    print "Player 1, what is your choice?"
+    tray_string1 = raw_input()
+    print "Player 2, what is your choice?"
+    tray_string2 = raw_input()
     print "So be it. Let the games begin!"
     winner = subprocess.call(game)
     if winner == 2:
-        print "We're going to do order 1 using user 2"
-        order = list()
-        items = tray_string1.split(",")
-        for item in items:
-            nums = item.split(":")
-            menu[nums[0]]['children'][nums[1]]['id']
+        makeOrder(menu,1,user2,tray_string1)
     elif winner == 3:
-        print "We're going to do order 2 using user 1"
+        makeOrder(menu,2,user1,tray_string2)
     else:
         print winner
         print "There was a problem with the game. Please try again later."
