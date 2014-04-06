0723a9502ef761ffa07bc07aa12e02f3de3eab2d
Daniel Bordak <dbordak@gmail.com>
2014-04-06 05:57:23 -0400

    Did a lot of things (sorry for the misuse of commits)

--- games/game.py
+++ games/game.py
@@ -1,6 +1,9 @@
-print "I am thinking of a number between 1 and 10. \n Whoever is closer is the winner."
-guess1 = raw_input("player 1 enter your guess:")
-guess2 = raw_input("player 2 enter your guess:")
+#!/bin/python2
+
+print "I am thinking of a number between 1 and 10."
+print "Whoever is closer is the winner."
+guess1 = raw_input("player 1 enter your guess: ")
+guess2 = raw_input("player 2 enter your guess: ")
 
 guess1= (int)(guess1)
 guess2= (int)(guess2)
@@ -11,18 +14,18 @@ ans1=abs(answer-guess1)
 ans2=abs(answer-guess2)
 
 while(ans1==ans2):
-  print "There is a tie, please try again"
+    print "There is a tie, please try again"
 
-  guess1 = raw_input("player 1 enter your guess:")
-  guess2 = raw_input("player 2 enter your guess:")
-  guess1= (int)(guess1)
-  guess2= (int)(guess2)
-  answer = ((guess1+guess2)*8) %10
+    guess1 = raw_input("player 1 enter your guess: ")
+    guess2 = raw_input("player 2 enter your guess: ")
+    guess1= (int)(guess1)
+    guess2= (int)(guess2)
+    answer = ((guess1+guess2)*8) %10
 
-  ans1=abs(answer-guess1)
-  ans2=abs(answer-guess2)
+    ans1=abs(answer-guess1)
+    ans2=abs(answer-guess2)
 
 if(ans1==0 or ans1<ans2):
-  print ("player 1 is the winner")
+    print ("player 1 is the winner")
 elif(ans2==0 or ans1>ans2):
-  print ("player 2 is the winner")
+    print ("player 2 is the winner")
