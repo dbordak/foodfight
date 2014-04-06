5d07adef1e57f3d06888145a940c142e80a8be1f
Daniel Bordak <dbordak@gmail.com>
2014-04-06 02:46:50 -0400

    Added createAccount

--- foodfight.py
+++ foodfight.py
@@ -1,6 +1,7 @@
 #!/bin/python2
 
 import subprocess
+import getpass
 import sys
 sys.path.append("pyordrin")
 import ordrin
@@ -9,17 +10,31 @@ GAME = "dummy"
 
 api = ordrin.APIs("G2emJ1PT6VbytwD9guV3mvEWEvV9TBd4r_Uh6PAm78E")
 
+def createAccount(playerNo):
+    print "What is player "+playerNo+"'s first name?"
+    f_name = raw_input()
+    print "What is player "+playerNo+"'s last name?"
+    l_name = raw_input()
+    print "What is player "+playerNo+"'s email?"
+    email = raw_input().lower()
+    print "What is player "+playerNo+"'s password? (WARNING: Not secure. Yet.)"
+    pw = getpass.getpass("")
+    ordrin.create_account(email, pw, f_name, l_name)
+
+
 def main():
+    print "Note: If applicable, the resident of your current location"
+    print "should be player 1."
     print "Does player 1 have an account? [Y/n]"
     ans = raw_input().lower()
     if ans == "n" or ans == "no":
-        player1 = createAccount()
+        player1 = createAccount("1")
     else:
         print "What is player 1's username?"
     print "Does player 2 have an account? [Y/n]"
     ans = raw_input().lower()
     if ans == "n" or ans == "no":
-        player2 = createAccount()
+        player2 = createAccount("2")
     else:
         print "What is player 2's username?"
 
